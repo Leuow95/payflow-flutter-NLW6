4999604f19e8fb731f4e71bc3c72a7be30024bf0
Leonardo Maia <leuow95@gmail.com>
2021-06-24 23:56:28 -0300

    excluuded flutter_lints and the unecessary packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/splash/splash_page.dart';
 
 import 'app_widget.dart';
-import 'modules/home/home_page.dart';
-import 'modules/login/login_page.dart';
-import 'shared/themes/app_colors.dart';
 
 void main() {
   runApp(AppFirebase());
