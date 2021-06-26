e2e65beb0bedb3fae0ce9ce0a42256d26eb075b9
Leonardo Maia <leuow95@gmail.com>
2021-06-26 17:12:54 -0300

    implemented setlabels, divider, bottomsheet, barcode_page and imported some packages

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcde_scanner/barcode_scanner_page.dart';
 
 import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
@@ -6,19 +8,27 @@ import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(
         primaryColor: AppColors.primary,
+        primarySwatch: Colors.orange,
       ),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
         "/login": (context) => LoginPage(),
         "/home": (context) => HomePage(),
+        "/barcode_scanner": (context) => BarCodeScannerPAge(),
       },
     );
   }
